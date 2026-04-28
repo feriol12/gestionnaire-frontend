37dce575ea2b57a3fde459f4ef7218628e107ed3
feriol12 <gnanviricardo@gmail.com>
2026-04-28 16:32:39 +0100

    configuration de guard

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import RegisterVue from '@/views/auth/RegisterVue.vue'
 import LoginVue from '@/views/auth/LoginVue.vue'
+import { useAuthStore } from '@/stores/useAuthStore'; 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,11 +16,14 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: RegisterVue,
+      meta: { guest: true } // accessible seulement si non connecté
+
     },
       {
       path: '/login',
       name: 'login',
       component: LoginVue,
+      meta: { guest: true } // accessible seulement si non connecté
     },
     {
       path: '/about',
@@ -30,6 +34,29 @@ const router = createRouter({
       component: () => import('../views/AboutView.vue'),
     },
   ],
+
 })
+
+
+
+// // Navigation Guard
+router.beforeEach(async (to, from, next) => {
+    const authStore = useAuthStore();
+    
+    // Vérifier si l'utilisateur est authentifié
+    const isAuthenticated = authStore.isAuthenticated;
+    
+    // Si la route nécessite l'auth
+    if (to.meta.requiresAuth && !isAuthenticated) {
+        next('/login');
+    }
+    // Si la route est pour invités et que l'utilisateur est connecté
+    else if (to.meta.guest && isAuthenticated) {
+        next('/dashboard');
+    }
+    else {
+        next();
+    }
+});
 
 export default router
